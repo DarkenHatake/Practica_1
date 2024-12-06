158013f99ea15f8b655d0e56bb171eab55aacec3
Алексей Кальдер <akaldertop05@gmail.com>
2024-12-06 11:07:42 +0700

    Разбиение по функциям

--- Practica_1/Practica_1.cpp
+++ Practica_1/Practica_1.cpp
@@ -16,30 +16,35 @@ struct sea {
     }
 };
 
-int main() {
-    // Пример входной строки
-    std::string input = "23.4  -2222.4542 2.4";
-    std::string input1 = "Dark  2222.366 2.4";
-    std::string input2 = "EERERvdv  2222.366 2.4";
-    // Регулярное выражение
+void building_of_the_object(std::string first, std::string second, std::string third) {
+    
+    std::string name = first;
+    double depth = std::stod(second);
+    double salinity = std::stod(third);
+
+    sea obj{ name, depth, salinity };
+
+    obj.display();
+}
+
+void seorch_str(std::string input) {
+    
     std::regex pattern("([a-zA-z]+)([\\s]+)([-+]?\[\\d.]+)([\\s]+)([-+]?\[\\d.]+)");
     std::smatch match;
-
-    // Проверка строки
-    if (std::regex_match(input1, match, pattern)) {
-        
-       
-        std::string name = match[1];
-        double depth = std::stod(match[3]);
-        double salinity = std::stod(match[5]);
-
-        sea obj{ name, depth, salinity };
-        // Выводим данные объекта
-        obj.display();
+    if (std::regex_match(input, match, pattern)) {
+        building_of_the_object(match[1], match[3], match[5]);
     }
     else {
         setlocale(LC_ALL, "RU");
         std::cout << "Ошибка: строка не соответствует ожидаемому формату." << std::endl;
     }
-   
+}
+
+int main() {
+    // Пример входной строки
+    std::string input = "23.4  -2222.4542 2.4";
+    std::string input1 = "Dark  2222.366 2.4";
+    std::string input2 = "EERERvdv  2222.366 2.4";
+
+    seorch_str(input2);
 }
